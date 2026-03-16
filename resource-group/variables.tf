eaa6adf66356cb042c6693eb58b542010b05f481
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-03-16 15:32:27 +0530

    created modules for resource group

---
+++ resource-group/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for resources"
+  type        = map(string)
+  default     = {}
+}
