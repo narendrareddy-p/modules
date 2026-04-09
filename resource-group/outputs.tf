e19e9c0c39c4ccf8594b97ff4ee63d3dea73f4f1
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-09 12:19:24 +0530

    updated

--- resource-group/outputs.tf
+++ resource-group/outputs.tf
@@ -1,15 +1,10 @@
 output "resource-group" {
   description = "Name of the resource group"
-  value = azurerm_resource_group.resource_group
+  value = azurerm_resource_group.resource_group.name
   
-}
-
-output "rg_id" {
-  description = "Resource Group ID"
-  value = azurerm_resource_group.rg.id
 }
 
 output "location" {
   description = "Resource Group Location"
-  value = azurerm_resource_group.rg.location
+  value = azurerm_resource_group.resource_group.location
 }
